6bebf7fb88cc6c2f1216bd35271b8a56c23b2d67
fengfive <zhuzifenghaha@gmail.com>
2017-05-03 17:23:37 +0800

    Finished project 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo(s)
+  s
+end
+
+def shout(s)
+  s.upcase
+end
+
+def repeat(s, t = 2)
+  "#{s}#{(" " + s) * (t - 1)}"
+end
+
+def start_of_word(s, num)
+  s[0...num]
+end
+
+def first_word(s)
+  s_array = s.split
+  s_array[0]
+end
+
+def titleize(s)
+  lowercase = ["and", "the", "of", "over"]
+  s_array = s.split
+  s_array.each_with_index.map do |i, index|
+    lowercase.include?(i) && index > 0 ? i : i.capitalize!
+  end
+  s_array.join(" ")
+end
