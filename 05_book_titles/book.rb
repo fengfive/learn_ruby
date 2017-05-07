53c773dbb7351cd5ab5f3072aa90ed13a72fa247
fengfive <zhuzifenghaha@gmail.com>
2017-05-07 11:05:12 +0800

    Finished project 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
 # write your code here
+  def title=(str)
+    split_title = str.split(" ")
+    split_title.each_with_index do |i, index|
+      lower_words = %w(a an and the in of)
+      if index == 0
+        i.capitalize!
+      elsif lower_words.include?(i)
+        i
+      else
+        i.capitalize!
+      end
+    end
+    @title = split_title.join(" ")
+  end
+
+  def title
+    @title
+  end
 end
